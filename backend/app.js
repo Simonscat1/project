3e02b12dfb472a8ea0af7408cf373556575c4387
Simon <kazakeev_roman@mail.ru>
2023-03-01 13:20:08 +0800

    123"

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const socketIO = require('socket.io')
+const http = require('http')
 
 const { discord } = require('./passports/index.js');
 const { auth, post } = require('./routes/index.js');
@@ -11,6 +13,10 @@ const { port, mongodb } = require("./config.js");
 
 
 const app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
+
+require('./soket/friend')(io)
 
 app.use(session({
     name: "web",
